caaf1ff03636ea3d458eb156449c7b8ba0bc0ba3
msksaul <64514098+msksaul@users.noreply.github.com>
2021-02-16 17:28:45 -0600

    Customize Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,52 @@
 import React from 'react'
-import { AppBar, Badge, Grid, InputBase, Toolbar, IconButton } from '@material-ui/core'
+import { AppBar, Badge, Grid, InputBase, Toolbar, IconButton, makeStyles } from '@material-ui/core'
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#fff'
+  },
+  searchInput: {
+    opacity: '0.6',
+    padding: '0px 8px',
+    fontSize: '0.8rem',
+    '&:hover': {
+      backgroundColor: '#f2f2f2'
+    },
+    '& .MuiSvgIcon-root': {
+      marginRight: '8px'
+    }
+  },
+})
 
 export default function Header() {
+
+  const classes = useStyles()
+
   return (
-    <AppBar position='static'>
+    <AppBar position='static' className={classes.root}>
       <Toolbar>
-        <Grid container>
+        <Grid container alignItems='center'>
           <Grid item>
-            <InputBase/>
+            <InputBase
+              className={classes.searchInput}
+              placeholder='Search topics'
+              startAdornment={<SearchIcon fontSize='small'/>}
+            />
           </Grid>
           <Grid item sm></Grid>
           <Grid item>
             <IconButton>
               <Badge badgeContent={4} color='secondary'>
-                <NotificationsNoneIcon/>
+                <NotificationsNoneIcon fontSize='small'/>
               </Badge>
             </IconButton>
             <IconButton>
               <Badge badgeContent={3} color='primary'>
-                <ChatBubbleOutlineIcon/>
+                <ChatBubbleOutlineIcon fontSize='small'/>
               </Badge>
             </IconButton>
             <IconButton>
