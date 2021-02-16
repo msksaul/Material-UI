29dcbfffaa50482337e172808a6ce72ddb13c67b
msksaul <64514098+msksaul@users.noreply.github.com>
2021-02-16 15:36:42 -0600

    Styling Components

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './App.css';
 import SideMenu from '../components/SideMenu'
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
+  } 
+})
 
 function App() {
+
+  const classes = useStyles()
+
   return (
     <>
     <SideMenu/>
-    <div>here we go</div>
+    <div className={classes.appMain}>here we go</div>
     </>
   );
 }
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core'
 
-export default function SideMenu() {
+const style = (
+  {
+    sideMenu : {
+      display: 'flex',
+      flexDirection: 'column',
+      position: 'absolute',
+      left: '0px',
+      width: '320px',
+      height: '100%',
+      backgroundColor: '#253053',
+    }
+  }
+)
+
+
+const SideMenu = (props) => {
+
+  const { classes } = props
+
   return (
-    <div className='side-menu'>
+    <div className={classes.sideMenu}>
       
     </div>
   )
 }
+
+export default withStyles(style)(SideMenu)
