4f19616af74631fe97d841af646ff25e21caea2c
msksaul <64514098+msksaul@users.noreply.github.com>
2021-02-16 15:20:45 -0600

    Initial Configuration

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import SideMenu from '../components/SideMenu'
+
+function App() {
+  return (
+    <>
+    <SideMenu/>
+    <div>here we go</div>
+    </>
+  );
+}
+
+export default App;
