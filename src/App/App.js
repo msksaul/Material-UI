4da49f8df72edc3d5aa9daa3125430e232426a6a
msksaul <64514098+msksaul@users.noreply.github.com>
2021-02-16 18:12:42 -0600

    Theme Customization

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,38 @@
 import React from 'react'
 import './App.css';
-import SideMenu from '../components/SideMenu'
+import SideMenu from '../components/SideMenu';
 import Header from '../components/Header';
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import PageHeader from '../components/PageHeader';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#333996',
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: '#f83245',
+      light: '#f8324526'
+    },
+    background: {
+      default: '#f5f5fd'
+    }
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translateZ(0)'
+      }
+    }
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain: {
@@ -16,13 +46,18 @@ function App() {
   const classes = useStyles()
 
   return (
-    <>
-    <SideMenu/>
-    <div className={classes.appMain}>
-      <Header/>
-    </div>
-    <CssBaseline/>
-    </>
+    <ThemeProvider theme={theme}>
+      <SideMenu/>
+      <div className={classes.appMain}>
+        <Header/>
+        <PageHeader
+          title='Page Header'
+          subtitle='Page description'
+          icon={<PeopleOutlineIcon fontSize='large'/>}
+        />
+      </div>
+      <CssBaseline/>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,22 @@ import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import SearchIcon from '@material-ui/icons/Search';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   searchInput: {
     opacity: '0.6',
-    padding: '0px 8px',
+    padding: `0px ${theme.spacing(1)}px`,
     fontSize: '0.8rem',
     '&:hover': {
       backgroundColor: '#f2f2f2'
     },
     '& .MuiSvgIcon-root': {
-      marginRight: '8px'
+      marginRight: theme.spacing(1)
     }
   },
-})
+}))
 
 export default function Header() {
 
